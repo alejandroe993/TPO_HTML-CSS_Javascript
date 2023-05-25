78eb685af2f4c481969cd3bd42d5170ff5d79c93
alejandroe993 <alejandroe993@gmail.com>
2023-05-25 01:45:10 -0300

    Cambios API

--- assets/js/validaformcaterin.js
+++ assets/js/validaformcaterin.js
@@ -1,23 +1,45 @@
+// Obtener referencias a los elementos del formulario
+const form = document.querySelector(".catering-form");
+const submitButton = document.querySelector("#submit");
 
-function validateEmail(email) {
-  const re = /\S+@\S+\.\S+/;
-  return re.test(email);
-}
+// Agregar un evento al botón de envío
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault(); // Evitar que el formulario se envíe automáticamente
 
-function validateForm() {
-  const nombre = document.getElementById("name").value.trim();
-  const email = document.getElementById("email").value.trim();
-  const mensaje = document.getElementById("message").value.trim();
+  // Validar los campos del formulario
+  const nombre = document.querySelector("#name").value.trim();
+  const email = document.querySelector("#email").value.trim();
+  const mensaje = document.querySelector("#message").value.trim();
 
   if (nombre === "" || email === "" || mensaje === "") {
-      alert("Por favor, complete todos los campos.");
-      return false;
+    alert("Por favor, complete todos los campos del formulario.");
+    return;
   }
 
-  if (!validateEmail(email)) {
-      alert("Por favor, ingrese un email válido.");
-      return false;
+  // Validar el formato del correo electrónico utilizando una expresión regular simple
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    alert("Por favor, ingrese un correo electrónico válido.");
+    return;
   }
 
-  return true;
-}
+  // Mostrar la ventana modal de agradecimiento
+  const modal = document.getElementById("modal");
+  modal.style.display = "block";
+});
+
+// Agregar un evento al botón de cerrar la ventana modal
+const closeButton = document.querySelector(".close");
+closeButton.addEventListener("click", function () {
+  // Cerrar la ventana modal
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
+});
+
+// Agregar un evento al botón de cerrar en la ventana modal
+const modalCloseButton = document.querySelector("#modal-close");
+modalCloseButton.addEventListener("click", function () {
+  // Cerrar la ventana modal
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
+});
